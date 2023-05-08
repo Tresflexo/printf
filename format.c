083ad55a468b9789b0c9ccaf9eac203f445365a6
angelina.engelmann@stud.thga.de <Engelmann>
2023-05-08 02:11:25 +0200

    fügt format.c, die eine Ganzzahl, eine Gleitkommazahl, einen Character und einen String mit printf() ausgibt, hinzu

---
+++ format.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int zahl = 42;
+    float gleitkommazahl = 3.14159;
+    char zeichen = 'X';
+    char name[] = "Max Mustermann";
+
+    printf("Ganzzahl: %d\n", zahl);
+    printf("Gleitkommazahl: %f\n", gleitkommazahl);
+    printf("Character: %c\n", zeichen);
+    printf("String: %s\n", name);
+
+    return 0;
+}
+
